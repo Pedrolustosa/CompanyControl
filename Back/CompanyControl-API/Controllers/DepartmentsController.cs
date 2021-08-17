d867085d5b9f10b0184ae0fff1b8ff2c66e0b992
Pedrolustosa <pedroeternalss@gmail.com>
2021-08-16 22:03:57 -0300

    method Get for DepartmentsController

--- Back/CompanyControl-API/Controllers/DepartmentsController.cs
+++ Back/CompanyControl-API/Controllers/DepartmentsController.cs
@@ -1,7 +1,30 @@
+using MongoDB.Driver;
+using Microsoft.AspNetCore.Mvc;
+using CompanyControl_API.Models;
+using Microsoft.Extensions.Configuration;
+
+
 namespace CompanyControl_API.Controllers
 {
-    public class DepartmentsController
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
     {
-        
+        private readonly IConfiguration _configuration;
+        public DepartmentsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeesAppCon"));
+
+            var dbList = dbClient.GetDatabase("testDB").GetCollection<Department>("Department").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
     }
 }
