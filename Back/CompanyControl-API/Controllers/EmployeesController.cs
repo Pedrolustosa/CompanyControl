80c6604d51a7aa16cb0f18bfc940557fc90ef3d3
Pedrolustosa <pedroeternalss@gmail.com>
2021-08-17 21:52:07 -0300

    method Get for EmployeesController

--- Back/CompanyControl-API/Controllers/EmployeesController.cs
+++ Back/CompanyControl-API/Controllers/EmployeesController.cs
@@ -1,7 +1,29 @@
+using MongoDB.Driver;
+using Microsoft.AspNetCore.Mvc;
+using CompanyControl_API.Models;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
 namespace CompanyControl_API.Controllers
 {
-    public class EmployeesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
     {
-        
+        private readonly IConfiguration _configuration;
+        public EmployeesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeesAppCon"));
+
+            var dbList = dbClient.GetDatabase("testDB").GetCollection<Employee>("Employee").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
     }
 }
