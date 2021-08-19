06e8a4e88649d9e58776888ed9b84728d6c7eef7
Pedrolustosa <pedroeternalss@gmail.com>
2021-08-19 20:42:17 -0300

    components Department, Employee, bootstrap and environment

--- Back/CompanyControl-API/Startup.cs
+++ Back/CompanyControl-API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
+using Microsoft.OpenApi.Models;
 
 namespace CompanyControl_API
 {
@@ -35,6 +36,10 @@ namespace CompanyControl_API
                 = new DefaultContractResolver());
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CompanyControl_API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +51,11 @@ namespace CompanyControl_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CompanyControl_API v1"));
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
